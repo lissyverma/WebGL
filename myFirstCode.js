b5df2ffcfe0a5a16612ea373a9fef3df95e355e7
lverma <lverma@walmartlabs.com>
2015-07-18 00:41:42 +0530

    asst1_beautify1

--- myFirstCode.js
+++ myFirstCode.js
@@ -12,15 +12,14 @@ window.onload = function init()
 	canvas = document.getElementById("gl-canvas");
 	gl = WebGLUtils.setupWebGL(canvas);
 	if (!gl) { alert("WebGL isn't available"); }
-	
-	   var vertices = [
+		
+		var vertices = [
         vec2( 0, 1 ),
         vec2( -0.8660254, -0.5),
         vec2( 0.8660254, -0.5)
     ];
-	
 	tesselate(vertices[0], vertices[1], vertices[2], subdivisions);
-	
+		
 	gl.viewport(0, 0, canvas.width, canvas.height);
 	gl.clearColor(0.0, 0.0, 0.0, 0.25);
 	
@@ -35,59 +34,10 @@ window.onload = function init()
 	gl.vertexAttribPointer(vposition, 2, gl.FLOAT, false, 0, 0);
 	gl.enableVertexAttribArray(vposition);
 	
-	 thetaLoc = gl.getUniformLocation( program, "theta" );
+	thetaLoc = gl.getUniformLocation( program, "theta" );
 
-    document.getElementById("Recursions" ).onclick = function(event) {
-        switch( event.srcElement.index ) {
-         case 0:
-            subdivisions = 5;
-            break;
-         case 1:
-            subdivisions = 6;
-            break;
-         case 2:
-            subdivisions = 7;
-            break;
-		 case 3:
-            subdivisions = 8;
-            break;
-         case 4:
-            subdivisions = 9;
-            break;
-         case 5:
-            subdivisions = 10;
-            break;
-		 case 6:
-            subdivisions = 11;
-            break;
-       }
-    };
-	
-	document.getElementById("Controls" ).onclick = function(event) {
-        switch( event.srcElement.index ) {
-         case 0:
-            theta = 1;
-            break;
-         case 1:
-            theta = 2;
-            break;
-         case 2:
-            theta = 3;
-            break;
-		 case 3:
-            theta = 4;
-            break;
-         case 4:
-            theta = 5;
-            break;
-         case 5:
-            theta = 6;
-            break;
-		 case 6:
-            theta = 7;
-            break;
-       }
-    };
+    document.getElementById("Button1").onclick = function(){theta += 1;};
+    document.getElementById("Button2").onclick = function(){theta -= 1;};
 	
 	render();
 };
@@ -121,6 +71,7 @@ function render()
 {
 	gl.uniform1f(thetaLoc, theta);
 	gl.clear(gl.COLOR_BUFFER_BIT);
+
 	gl.drawArrays(gl.TRIANGLES, 0, points.length);
 	setTimeout(
         function (){requestAnimFrame(render);}, 1);
